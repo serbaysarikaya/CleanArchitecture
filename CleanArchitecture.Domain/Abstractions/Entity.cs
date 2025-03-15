29f17ee8c9c96826a6433eca17d256f405e62ef9
Serbay <ssarikaya@tersan.com.tr>
2025-03-15 13:50:31 +0300

    fixed

--- CleanArchitecture.Domain/Abstractions/Entity.cs
+++ CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -7,7 +7,6 @@ namespace CleanArchitecture.Domain.Abstractions;
 public abstract class Entity
 {
     public string Id { get; set; } = null!;
-
     public DateTime CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
 }
