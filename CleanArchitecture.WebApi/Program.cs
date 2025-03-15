4d827d6bc416645b2dbdf7f84893bd2c82a2817e
Serbay <ssarikaya@tersan.com.tr>
2025-03-15 13:07:44 +0300

    Project Architecture created

--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,17 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. Assembyl refers to the CleanArchitecture.Presentation project
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "CleanArchitecture.WebApi", Version = "v1" });
+});
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArchitecture.WebApi v1"));
 }
 
 app.UseHttpsRedirection();
