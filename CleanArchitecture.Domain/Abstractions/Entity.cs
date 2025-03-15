d68da9cd6b2133b4c2273d3d728c8a11789adcc6
Serbay <ssarikaya@tersan.com.tr>
2025-03-15 13:50:17 +0300

    Db Enttiy

---
+++ CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Abstractions;
+
+public abstract class Entity
+{
+    public string Id { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,6 +1,18 @@
+
+
+using CleanArchitecture.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+string connectionString = builder.Configuration.GetConnectionString("SqlServer");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+
 // Add services to the container. Assembyl refers to the CleanArchitecture.Presentation project
+
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
 
@@ -10,10 +22,7 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new() { Title = "CleanArchitecture.WebApi", Version = "v1" });
 });
 
-
-
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
